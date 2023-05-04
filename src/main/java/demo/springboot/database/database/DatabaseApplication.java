f7e28da4f36220f9564757fc1750c42fe3488c0e
vphonap <vphonap@gmail.com>
2023-05-04 14:15:28 +0530

    derby dependencies

--- src/main/java/demo/springboot/database/database/DatabaseApplication.java
+++ src/main/java/demo/springboot/database/database/DatabaseApplication.java
@@ -2,7 +2,11 @@ package demo.springboot.database.database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = {"demo.springboot.database.*"})
+//@ComponentScan(basePackages = { "demo.springboog.database.*" })
+//@EntityScan(basePackages = {"demo.mypackage.package.entity"})
 @SpringBootApplication
 public class DatabaseApplication {
 

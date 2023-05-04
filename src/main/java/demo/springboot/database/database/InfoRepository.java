656f20f6635da747d2adf799d0e1ac9860c249c4
vphonap <vphonap@gmail.com>
2023-05-04 14:13:42 +0530

    unresolved derby loader issue

--- src/main/java/demo/springboot/database/database/InfoRepository.java
+++ src/main/java/demo/springboot/database/database/InfoRepository.java
@@ -1,7 +1,10 @@
 package demo.springboot.database.database;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+//@Repository
 public interface InfoRepository extends CrudRepository<Info,String> {
     
 }
+
